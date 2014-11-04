9f33efd9eb4718afac4702082ba5c4a3b0416fce
Simon <simon@bitcartel.com>
2014-11-03 21:53:49 -0800

    Reduce time interval for checking assets.

--- src/main/java/org/multibit/viewsystem/swing/UpdateAssetsTimerTask.java
+++ src/main/java/org/multibit/viewsystem/swing/UpdateAssetsTimerTask.java
@@ -41,7 +41,7 @@ import org.coinspark.wallet.CSBalanceDatabase;
 
 
 public class UpdateAssetsTimerTask extends TimerTask {
-    public static final int INTERVAL = 15000; // milliseconds
+    public static final int INTERVAL = 2000; // milliseconds
 
     private static Logger log = LoggerFactory.getLogger(UpdateAssetsTimerTask.class);
 
