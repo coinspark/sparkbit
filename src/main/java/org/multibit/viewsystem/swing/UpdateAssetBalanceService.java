307300a788542967dcb22008adee45b414a9ef86
Simon <simon@bitcartel.com>
2014-12-20 18:09:32 -0800

    - Fix possible ConcurrentModificationException when model wallet list changes while iterator is being used. - If a wallet has a replay task, skip updating.

--- src/main/java/org/multibit/viewsystem/swing/UpdateAssetBalanceService.java
+++ src/main/java/org/multibit/viewsystem/swing/UpdateAssetBalanceService.java
@@ -31,10 +31,10 @@ import java.util.*;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+public enum UpdateAssetBalanceService {
 
-public enum UpdateAssetBalanceService  {
     INSTANCE;
-    
+
     public static final int INTERVAL = 5000; // milliseconds
 
     private static Logger log = LoggerFactory.getLogger(UpdateAssetBalanceService.class);
@@ -44,22 +44,20 @@ public enum UpdateAssetBalanceService  {
 
 //    private boolean updateTransactions = false;
 //    private boolean isCurrentlyUpdating = false;
-
     //private ExecutorService pool; // single executor thread, tasks processed in sequence
     private ScheduledThreadPoolExecutor pool;
-    
+
     public void initalize(BitcoinController bitcoinController) {
-        //this.controller = bitcoinController;
-        this.bitcoinController = bitcoinController;
-        this.pool = new ScheduledThreadPoolExecutor(1); //Executors.newSingleThreadExecutor();
-	
+	//this.controller = bitcoinController;
+	this.bitcoinController = bitcoinController;
+	this.pool = new ScheduledThreadPoolExecutor(1); //Executors.newSingleThreadExecutor();
+
 	try {
 	    this.pool.scheduleWithFixedDelay(getRunnable(), INTERVAL, INTERVAL, TimeUnit.MILLISECONDS);
 	} catch (Exception e) {
 	    // uh-oh
 	}
     }
-    
 
     //@Override
 //    public void cancel() {
@@ -67,7 +65,6 @@ public enum UpdateAssetBalanceService  {
 //        this.pool.shutdownNow();
 //        //return super.cancel();
 //    }
-    
     public boolean cancelAndAwaitTermination() {
 	boolean result = false;
 	try {
@@ -78,54 +75,86 @@ public enum UpdateAssetBalanceService  {
 	}
 	return result;
     }
-    
+
     // TODO: Better for task to iterate over wallets and invoke update methods
     //       or add a separate task for each wallet to the executor queue?
 //    @Override
     public Runnable getRunnable() {
-        return new Runnable() {
-            @Override
-            public void run() {
-                log.debug("UpdateAssetBalanceService fired and executing");
-		
-		if (pool.isShutdown()) return;
-		
-                PeerGroup peerGroup = null;
-                if (bitcoinController.getMultiBitService() != null && bitcoinController.getMultiBitService().getPeerGroup() != null) {
-                    peerGroup = bitcoinController.getMultiBitService().getPeerGroup();
-                }
+	return new Runnable() {
+	    @Override
+	    public void run() {
+		log.debug("UpdateAssetBalanceService fired and executing");
 
-                List<WalletData> perWalletModelDataList = bitcoinController.getModel().getPerWalletModelDataList();
-                if (perWalletModelDataList != null) {
-                    Iterator<WalletData> iterator = perWalletModelDataList.iterator();
-                    while (iterator.hasNext()) {
-			if (pool.isShutdown()) return;
-			try {
-			    WalletData wd = iterator.next();
-			    if (wd != null) {
-//                            log.debug("LOOP: Attempting to update assets in wallet: " + wd.getWalletDescription() + "...");
-				Wallet w = wd.getWallet();
-				if (w == null) {
-				    log.debug("getWallet() returned null");
-				} else {
-				    if (peerGroup == null) {
-					log.debug("Cannot invoke validateAssets(peerGroup) as peerGroup is null");
-				    } else {
-					if (pool.isShutdown()) return;
-					w.CS.validateAllAssets(peerGroup);
-				    }
-				    if (pool.isShutdown()) return;
-				    w.CS.calculateBalances();
-				}
+		if (pool.isShutdown()) {
+		    return;
+		}
+
+		try {
+
+		    PeerGroup peerGroup = null;
+		    if (bitcoinController.getMultiBitService() != null && bitcoinController.getMultiBitService().getPeerGroup() != null) {
+			peerGroup = bitcoinController.getMultiBitService().getPeerGroup();
+		    }
+//		log.info("....timer: getPeerGroup()");
+
+		    List<WalletData> perWalletModelDataList = bitcoinController.getModel().getPerWalletModelDataList();
+		    if (perWalletModelDataList != null) {
+			// iterator can throw ConcurrentModificationException so we iterate over copy
+			List<WalletData> shallowCopy = new ArrayList<>(perWalletModelDataList);
+			Iterator<WalletData> iterator = shallowCopy.iterator();
+			while (iterator.hasNext()) {
+			    if (pool.isShutdown()) {
+				return;
 			    }
-			} catch (java.util.ConcurrentModificationException cme) {
+			    try {
+				WalletData wd = iterator.next();
+
+				List<WalletData> latest = bitcoinController.getModel().getPerWalletModelDataList();
+				if (latest == null || !latest.contains(wd)) {
+				    continue; // this wallet data no longer exists in model so carry on looping 
+				}
+
+				if (wd != null) {
+//                            log.debug("LOOP: Attempting to update assets in wallet: " + wd.getWalletDescription() + "...");
+				    UUID uuid = wd.getReplayTaskUUID();
+				if (uuid!=null) {
+//				    log.debug("....timer: Wallet has replay task: " + wd.getWalletFilename());
+				    continue;
+				}
+				    Wallet w = wd.getWallet();
+				    if (w == null) {
+					log.debug("getWallet() returned null");
+				    } else {
+					if (peerGroup == null) {
+					    log.debug("Cannot invoke validateAssets(peerGroup) as peerGroup is null");
+					} else {
+					    if (pool.isShutdown()) {
+						return;
+					    }
+//					log.info("....timer: start validateAllAssets()");
+					    w.CS.validateAllAssets(peerGroup);
+//					log.info("....timer: finish validateAllAssets()");
+					}
+					if (pool.isShutdown()) {
+					    return;
+					}
+//					log.info("....timer: start calculateBalances()");
+					w.CS.calculateBalances();
+//					log.info("....timer: finish calculateBalances()");
+				    }
+				}
+			    } catch (java.util.ConcurrentModificationException cme) {
 //			    log.debug("Caught ConcurrentModificationException: " + cme.getMessage());
+			    }
 			}
 		    }
-                }
-                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        };
+		    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		} catch (Exception e) {
+		    // if uncaught, an exception will kill the scheduled executor
+		    log.error("Update asset balance service: Caught exception: " + e);
+		}
+	    }
+	};
     }
 
 }
